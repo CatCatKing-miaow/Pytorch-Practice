b5176ffab719233689bf6d8fca6b0eee61d6468e
unknown <2291256750@qq.com>
2026-02-15 22:00:49 +0800

    reviewing the Basics

---
+++ 李沐课程实操/线性回归的实现重点.py
@@ -0,0 +1,13 @@
+import torch
+import numpy as np
+from torch.utils import data
+import random
+def synthetic_data(w,b,num_examples):
+    x=torch.normal(0,1,(num_examples,len(w)))
+    y=torch.matmul(x,w)+b
+    y+=torch.normal(0,0.01,y.shape)
+    return x,y.reshape((-1,1))#手动reshape防止广播机制隐患
+
+true_w = torch.tensor([2,-3.4])#w是一维向量，但是matmul能够智能处理
+true_b = 4.2
+features,labels=synthetic_data(true_w,true_b,1000)#获取带标签的数据
--- 李沐课程实操/自动求导实现.py
+++ 李沐课程实操/自动求导实现.py
@@ -1,1 +1,12 @@
 import torch
+x= torch.arange(4.0)
+x.requires_grad_(True)
+y=2*torch.dot(x,x)
+
+y.backward()
+x.grad.zero_()   #梯度清零
+y=x*x            #哈达玛积
+u=y.detach()     #把u赋值为常数，值为y
+z=u*x
+z.sum().backward()#不直接对z反向传播，不然会鼓捣出来一个雅可比矩阵
+print(x.grad)
